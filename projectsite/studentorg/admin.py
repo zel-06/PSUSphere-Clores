d85ec054b8b550a8c5fa4babf678e7ea87598f89
Ryan Jazer Prieto <prietoryanjazer@gmail.com>
2026-02-16 14:58:58 +0800

    Task A - CollegeAdmin(modified)

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from .models import College, Program, Organization, Student, OrgMember
 
-admin.site.register(College)
+@admin.register(College)
+class CollegeAdmin(admin.ModelAdmin):
+    list_display = ("college_name", "created_at", "updated_at")
+    search_fields = ("college_name",)
+    list_filter = ("created_at",)
+
 admin.site.register(Program)
 admin.site.register(Organization)
 
@@ -24,8 +29,5 @@ class OrgMemberAdmin(admin.ModelAdmin):
         except Student.DoesNotExist:
             return None
         
-@admin.register(College)
-class CollegeAdmin(admin.ModelAdmin):
-    list_display = ("college_name", "created_at", "updated_at")
-    search_fields = ("college_name",)
-    list_filter = ("created_at",)
+
+
