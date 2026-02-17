0ad138f4af9c35d87f20c5cf37c1e04d387418a3
zel-06 <cymonjezrelb@gmail.com>
2026-02-17 21:28:45 +0800

    modify studentorg\veiws.py

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.list import ListView
+from studentorg.models import Organization
+
+class HomePageView(ListView):
+    model = Organization
+    context_object_name = 'home'
+    template_name = "home.html"
