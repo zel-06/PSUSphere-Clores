267b518483b1f8d3d7a77438d2751cb3502472d4
Ryan Jazer Prieto <prietoryanjazer@gmail.com>
2026-02-17 21:36:05 +0800

    modify projectsite/urls.py

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from studentorg.views import HomePageView
+from studentorg import views
+
+urlpatterns = [
+path("admin/", admin.site.urls),
+path('', views.HomePageView.as_view(), name='home'),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
