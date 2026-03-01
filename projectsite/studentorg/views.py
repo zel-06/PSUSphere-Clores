6f45b2d9eacea7276afa8072d6788e11f1891efa
zel-06 <cymonjezrelb@gmail.com>
2026-03-02 01:22:51 +0800

    Dashboard implementation

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -8,6 +8,7 @@ from studentorg.models import Organization, OrgMember, Student, College, Program
 from studentorg.forms import OrganizationForm, OrgMemberForm, StudentForm, CollegeForm, ProgramForm
 from django.urls import reverse_lazy
 from django.db.models import Q
+from django.utils import timezone
 
 #Organization
 class OrganizationCreateView(CreateView):
@@ -20,6 +21,22 @@ class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+
+        today = timezone.now() .date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+        context["students_joined_this_year"] = count
+        return context
 
 class OrganizationList(ListView):
     model = Organization
